b1042d9e0dbd0d961b3397ddcec998432df458fb
Vlagimir Ivanov <volnyivolodya@gmail.com>
2024-10-09 16:39:52 +0300

    Delete app directory

--- app/error.py
+++
@@ -1,8 +0,0 @@
-class InvalidInputException(Exception):
-    def __init__(self, f: callable, *args):
-        super().__init__()
-        self.f = f
-        self.args = args
-
-    def __str__(self):
-        return f"Values {self.args} violate {self.f.__name__}'s function domain"
